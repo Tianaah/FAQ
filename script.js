64fc8e3d191e7053bfb4e5bd891d1d8e95266461
Tianaah <christiana15ako@gmail.com>
2024-11-07 18:56:30 +0100

    faq

---
+++ script.js
@@ -0,0 +1,10 @@
+const question= document.queryselector('.question');
+const plus= document.querySelector('.plus');
+const minus= document.querySelector('.minus');
+const para= document.querySelector('.para');
+
+
+document.addEventListener("click", function(){
+   console.log(para, "clicked");
+   console.log('test')
+  });
